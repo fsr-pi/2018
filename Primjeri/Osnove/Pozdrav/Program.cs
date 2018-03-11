07fb73da52d29378675ca0959832d7a2bb1d4b8f
Boris Milasinovic <boris.milasinovic@fer.hr>
2018-03-12 00:15:37 +0100

    Dodani uvodni primjeri

---
+++ Primjeri/Osnove/Pozdrav/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Pozdrav
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.OutputEncoding = Encoding.UTF8;      
+
+      Console.WriteLine("Pozdrav! Unesite tekst");
+     
+      string line = Console.ReadLine();
+      Console.WriteLine("Upisani tekst je " + line);
+      Console.WriteLine("Upisani tekst je {0}",  line);
+      Console.WriteLine($"Upisani tekst je {line}");
+    }
+  }
+}
