f44510bed10f46a1bc438bb3fc896767f3124e8e
Boris Milasinovic <boris.milasinovic@fer.hr>
2018-04-09 20:41:30 +0200

    Inicijalni primjer za web aplikaciju (osnovne postavke + CRUD za Državu)

---
+++ Primjeri/Web/Firma.Mvc/Models/Mjesto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Firma.Mvc.Models
+{
+    public partial class Mjesto
+    {
+        public Mjesto()
+        {
+            PartnerIdMjestaIsporukeNavigation = new HashSet<Partner>();
+            PartnerIdMjestaPartneraNavigation = new HashSet<Partner>();
+        }
+
+        public int IdMjesta { get; set; }
+        public string OznDrzave { get; set; }
+        public string NazMjesta { get; set; }
+        public int PostBrMjesta { get; set; }
+        public string PostNazMjesta { get; set; }
+
+        public Drzava OznDrzaveNavigation { get; set; }
+        public ICollection<Partner> PartnerIdMjestaIsporukeNavigation { get; set; }
+        public ICollection<Partner> PartnerIdMjestaPartneraNavigation { get; set; }
+    }
+}
