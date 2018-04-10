28cdbc4ba234516fe45eab1a8a1e69b79e24dbb5
Boris Milasinovic <boris.milasinovic@fer.hr>
2018-04-10 21:03:33 +0200

    Prikaz svih mjesta (bez straničenja na serveru) - primjer za agregirani ViewModel

---
+++ Primjeri/Web/Firma.Mvc/Controllers/MjestoController.cs
@@ -0,0 +1,40 @@
+﻿using Firma.Mvc.Extensions;
+using Firma.Mvc.Models;
+using Firma.Mvc.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Firma.Mvc.Controllers
+{
+  public class MjestoController : Controller
+  {
+    private readonly FirmaContext ctx;
+    private readonly AppSettings appData;
+
+    public MjestoController(FirmaContext ctx, IOptions<AppSettings> options)
+    {
+      this.ctx = ctx;
+      appData = options.Value;
+    }
+
+    public IActionResult Index()
+    {
+      var mjesta = ctx.Mjesto
+                      .Select(m => new MjestoViewModel
+                      {
+                        IdMjesta = m.IdMjesta,
+                        NazivMjesta = m.NazMjesta,
+                        PostBrojMjesta = m.PostBrMjesta,
+                        PostNazivMjesta = m.PostNazMjesta,
+                        NazivDrzave = m.OznDrzaveNavigation.NazDrzave
+                      })                      
+                      .ToList();
+      return View(mjesta);
+    }
+  }
+}
